f64476d2a585f167eaceb4c08fb85654068317b7
Jovy83 <jovy83@live.com>
2020-09-07 22:08:38 +0800

    Setup startRemoveExpense

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,16 +3,16 @@ import React from "react";
 import { EditExpensePage } from "../../components/EditExpensePage";
 import expenses from "../fixtures/expenses"; // we'll use the test expenses data instead of the store
 
-let editExpense, removeExpense, history, wrapper;
+let editExpense, startRemoveExpense, history, wrapper;
 
 beforeEach(() => {
   editExpense = jest.fn(); // spy
-  removeExpense = jest.fn(); // spy
+  startRemoveExpense = jest.fn(); // spy
   history = { push: jest.fn() }; // object with spy property
   wrapper = shallow(
     <EditExpensePage
       editExpense={editExpense}
-      removeExpense={removeExpense}
+      startRemoveExpense={startRemoveExpense}
       history={history}
       expense={expenses[2]}
     />
@@ -29,10 +29,10 @@ test("should handle editExpense", () => {
   expect(editExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
 });
 
-test("should handle removeExpense", () => {
+test("should handle startRemoveExpense", () => {
   wrapper.find("button").simulate("click");
   expect(history.push).toHaveBeenLastCalledWith("/");
-  expect(removeExpense).toHaveBeenLastCalledWith({
+  expect(startRemoveExpense).toHaveBeenLastCalledWith({
     id: expenses[2].id,
   });
 });
