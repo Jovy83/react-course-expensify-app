e6bdc201f573498b9852e658ad93e6453df5d754
Jovy83 <jovy83@live.com>
2020-09-17 10:14:16 +0800

    Add Expensify styles

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = (env, argv) => {
   const CSSExtract = new MiniCssExtractPlugin({ filename: "styles.css" });
 
   return {
-    entry: "./src/app.js",
+    entry: ["babel-polyfill", "./src/app.js"],
     output: {
       path: path.join(__dirname, "public", "dist"),
       filename: "bundle.js",
@@ -45,6 +45,7 @@ module.exports = (env, argv) => {
               loader: "css-loader",
               options: {
                 sourceMap: true,
+                url: false,
               },
             },
             {
